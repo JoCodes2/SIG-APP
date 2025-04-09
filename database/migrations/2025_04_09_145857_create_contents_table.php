ca58ea815e045a3e7cf368323f24184d9e0367bb
divta-suryawan <gitdivta@gmail.com>
2025-04-09 23:00:05 +0800

    setup migrations

---
+++ database/migrations/2025_04_09_145857_create_contents_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('contents', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->string('image')->nullable();
+            $table->text('link')->nullable();
+            $table->enum('category', ['galeri', 'news', 'link']);
+            $table->date('date_upload');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('contents');
+    }
+};
---
+++ database/migrations/2025_04_09_145857_create_members_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('members', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('name');
+            $table->date('date_birth');
+            $table->string('place_birth');
+            $table->integer('age');
+            $table->text('address');
+            $table->enum('status', ['widow', 'single', 'marry', 'widower']);
+            $table->enum('status_member', ['youth', 'pastor', 'administrator']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('members');
+    }
+};
