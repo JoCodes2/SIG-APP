a92be8bb8ae409184e702ceb2bd58a5ec0eaf405
akbar tri wicaksono <akbartriwicaksono46@gmail.com>
2025-04-09 22:48:57 +0800

    first

---
+++ resources/views/Admin/dashboard.blade.php
@@ -0,0 +1,9 @@
+@extends('Layouts.Base')
+
+@section('content')
+    <h1>WEHHH LO NGAPAIN ??!</h1>
+    <h1>Pi Istirahat Jo Kasihan</h1>
+@endsection
+
+@section('scripts')
+@endsection
---
+++ resources/views/Layouts/Sidebar.blade.php
@@ -0,0 +1,37 @@
+        <!-- Menu -->
+
+        <aside id="layout-menu" class="layout-menu menu-vertical menu bg-menu-theme">
+            <div class="app-brand demo">
+                <a href="index.html" class="app-brand-link">
+                    <span class="app-brand-logo demo">
+                        <img src="{{ asset('assets/assets/logogereja.jpeg') }}" alt="Logo" class="img-fluid"
+                            width="50" height="50">
+                    </span>
+                    <span class="text-start app-brand-text fw-bold ms-2"
+                        style="color: red; text-shadow: 1px 1px 2px black;">
+                        <small>SISTEM</small><br>
+                        <small>INFORMASI</small><br>
+                        <small>GEREJA</small>
+                    </span>
+
+                </a>
+
+                <a href="javascript:void(0);" class="layout-menu-toggle menu-link text-large ms-auto d-block d-xl-none">
+                    <i class="bx bx-chevron-left bx-sm align-middle"></i>
+                </a>
+            </div>
+
+            <div class="menu-inner-shadow"></div>
+
+            <ul class="menu-inner py-1">
+                <li class="menu-item {{ request()->is('dashboard') ? 'active' : '' }}">
+                    <a href="/dashboard" class="menu-link">
+                        <i class="menu-icon fa-solid fa-house"></i>
+                        <div data-i18n="Analytics">Dashboard</div>
+                    </a>
+                </li>
+
+
+            </ul>
+        </aside>
+        <!-- / Menu -->
